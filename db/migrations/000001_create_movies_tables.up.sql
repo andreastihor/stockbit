ebfacc4d2d1edee5c84772c7107289eacf1eafc6
andreastihor <andreastihor@gmail.com>
2021-10-24 20:42:34 +0700

    refactor: removing unique key to capture all search history

--- db/migrations/000001_create_movies_tables.up.sql
+++ db/migrations/000001_create_movies_tables.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS movies(
    id serial PRIMARY KEY,
-   title VARCHAR (50) UNIQUE NOT NULL,
+   title VARCHAR (50)  NOT NULL,
    type VARCHAR (10) NOT NULL,
    poster VARCHAR (300)  NOT NULL,
-   imdb_id VARCHAR (10) UNIQUE NOT NULL,
+   imdb_id VARCHAR (10)  NOT NULL,
    year VARCHAR (10)  NOT NULL
 );
