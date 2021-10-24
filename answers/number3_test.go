ac9eae66c4d56903811f4750f42ab87f679d676f
andreastihor <andreastihor@gmail.com>
2021-10-24 21:04:14 +0700

    feat: changing test to use assertion

--- answers/number3_test.go
+++ answers/number3_test.go
@@ -3,18 +3,16 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFindStringInsideFirstBracketSuccess(t *testing.T) {
 	result := FindStringInsideFirstBracket("disini (ada) kurungan")
-	if result != "ada" {
-		t.Errorf(fmt.Sprintf("Wrong result : %s, expected = 'ada'", result))
-	}
+	assert.Equal(t, result, "ada", fmt.Sprintf("Wrong result : %s, expected = 'ada'", result))
 }
 
 func TestFindStringInsideFirstBracketFail(t *testing.T) {
-	result := FindStringInsideFirstBracket("disini (ada k)urungan")
-	if result == "ada" {
-		t.Error(fmt.Sprintf("Wrong result : %s, expected = 'ada k", result))
-	}
+	result := FindStringInsideFirstBracket("disini (ada)urungan")
+	assert.NotEqual(t, result, "adaa", fmt.Sprintf("Wrong result : %s, expected = 'ada k'", result))
 }
--- answers/number4_test.go
+++ answers/number4_test.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAnagramSuccess(t *testing.T) {
 	groupedWords := Anagram()
-	if len(groupedWords) != 4 {
-		t.Errorf(fmt.Sprintf("Wrong result : %d, expected = '4'", len(groupedWords)))
-	}
+	assert.Equal(t, len(groupedWords), 4, fmt.Sprintf("Wrong result : %d, expected = '4'", len(groupedWords)))
+}
+
+func TestAnagramFail(t *testing.T) {
+	groupedWords := Anagram()
+	assert.NotEqual(t, len(groupedWords), 5, fmt.Sprintf("Wrong result : %d, expected = '4'", len(groupedWords)))
 }
