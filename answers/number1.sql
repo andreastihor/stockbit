b36ef0cf4ee8f0e4ab88c6af97001a527ba23901
andreastihor <andreastihor@gmail.com>
2021-10-24 21:30:36 +0700

    refactor: swapping the join alias for better readability

--- answers/number1.sql
+++ answers/number1.sql
@@ -1,7 +1,7 @@
 SELECT Parent.id    AS "ID",
        Parent.NAME  AS "UserName",
        Child."name" AS "ParentUserName"
-FROM   USER AS Parent
-       LEFT JOIN USER AS Child
+FROM   USER AS Child
+       RIGHT JOIN USER AS Parent
               ON Parent.parent_id = Child.id
-ORDER  BY "ID" 
+ORDER  BY "ID" ;
