15a0a1c6d589e8237dda4cf4954333a0e466ecf8
andreastihor <andreastihor@gmail.com>
2021-10-25 19:51:27 +0700

    feat: adding unit test for service level

--- pkg/movie/service_test.go
+++ pkg/movie/service_test.go
@@ -6,20 +6,14 @@ import (
 	"github.com/andreastihor/stockbit/models"
 	"github.com/andreastihor/stockbit/pkg/movie/mocks"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestGetMovies(t *testing.T) {
 	repo := mocks.RepositoryInterface{}
-	service := NewService(&Repository{})
-	mockMovie := models.Movie{
-		Title:  "testing title",
-		Year:   "1998",
-		ImdbID: "123dasd",
-		Type:   "movie",
-		Poster: "https://m.media-amazon.com/images/M/MV5BYTEzMmE0ZDYtYWNmYi00ZWM4LWJjOTUtYTE0ZmQyYWM3ZjA0XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg",
-	}
-	repo.On("InsertMovie", mockMovie).Return(mockMovie).Once()
+	service := NewService(&repo)
 
+	repo.On("InsertMovie", mock.Anything).Return(nil)
 	mockParams := models.Params{
 		Pagination: "1",
 		Search:     "batman",
